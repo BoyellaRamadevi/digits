aa95a22fbe0d81a0fa622be0031ce97e0b844288
BoyellaRamadevi <41728410+BoyellaRamadevi@users.noreply.github.com>
2018-07-30 14:52:59 +0530

    Create digits.java

---
+++ digits.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+
+
+
+        int count = 0, num;
+        Scanner sc=new Scanner(System.in);
+        num=sc.nextInt();
+
+        for(; num != 0; num/=10, ++count) {   
+        }
+
+        System.out.println(count);
+    }
+}
+	
